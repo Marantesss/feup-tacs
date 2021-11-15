803fb6068ce82d5ca7ed92277b74fdb4d7d610fa
Marantesss <up201706917@edu.fe.up.pt>
2021-11-15 16:19:35 +0000

    :poop: i tried :(

--- factory.ts
+++ factory.ts
@@ -1,19 +1,69 @@
+interface Factory {
+  withSugar(): Factory & SugarFactory;
+  withWhey(): Factory & WheyFactory;
+  withProtein(): Factory & ProteinFactory;
+}
+
+interface SugarFactory extends Factory {
+  getSugar(): number;
+}
+
+interface WheyFactory extends Factory {
+  getWhey(): boolean;
+}
+
+interface ProteinFactory extends Factory {
+  getProtein(): "A" | "B";
+}
+
+const getSugar = (): number => 0;
+const getProtein = (): "A" | "B" => "A";
+const getWhey = (): boolean => false;
+
+const withSugar = (): Factory & SugarFactory => ({ ...factory, getSugar });
+const withWhey = (): Factory & WheyFactory => ({ ...factory, getWhey })
+const withProtein = (): Factory & ProteinFactory => ({ ...factory, getProtein })
+
+const factory: Factory = {
+  withSugar,
+  withWhey,
+  withProtein,
+};
+
+factory.withProtein().withSugar().getSugar(); //?
+
+/**
+
 class Factory {
-    withSugar(): Factory & { getSugar(): number } { return null }
-    withWhey(): Factory & { getWhey(): boolean } { return null }
-    withProtein(): Factory { return null } //?
+  withSugar(): Factory & { getSugar(): number } {
+    return new SugarFactory();
   }
-  
-  class SugarFactory extends Factory {
-    getSugar(): number { return 0 }
+  withWhey(): Factory & { getWhey(): boolean } {
+    return new WheyFactory();
   }
-  
-  class WheyFactory extends Factory {
-    getWhey(): boolean { return false }
+  withProtein(): Factory & { getProtein(): "A" | "B" } {
+    return new ProteinFactory();
   }
-  
-  class ProteinFactory extends Factory {
-    getProtein(): 'A' | 'B' { return 'A' }
+}
+
+class SugarFactory extends Factory {
+  getSugar(): number {
+    return 0;
   }
-  
-  new Factory().withWhey().getWhey() //?
+}
+
+class WheyFactory extends Factory {
+  getWhey(): boolean {
+    return false;
+  }
+}
+
+class ProteinFactory extends Factory {
+  getProtein(): "A" | "B" {
+    return "A";
+  }
+}
+
+new Factory().withWhey().withSugar(); //?
+
+*/
