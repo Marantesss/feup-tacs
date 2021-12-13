6a7ddb74154cc4fce880fa24a418c48e829c95dd
César Nogueira <github@cesarnogueira.com>
2021-12-13 15:44:37 +0000

    Working timed translation

--- animator/animation-test.js
+++ animator/animation-test.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var raf;
-var translation = { x: 200, y: 250 };
+var starttime;
+var translation = { x: 200, y: 250, time: 2000 };
 
 var circle = {
     x: 100,
@@ -19,17 +20,29 @@ var circle = {
     }
 };
 
-function translate() {
+function translate(timestamp, x0, y0) {
+    var timestamp = timestamp || new Date().getTime();
+    var runtime = timestamp - starttime;
+    var progress = runtime / translation.time;
+    progress = Math.min(progress, 1);
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     circle.draw();
-    if (circle.x != translation.x)
-        circle.x += circle.vx;
-    if (circle.y != translation.y)
-        circle.y += circle.vy;
-    raf = window.requestAnimationFrame(translate);
+
+    circle.x = x0 + (distX * progress);
+    circle.y = y0 + (distY * progress);
+
+    if (runtime < translation.time) {
+        raf = window.requestAnimationFrame(function (timestamp) {
+            translate(timestamp, x0, y0);
+        });
+    }
 }
 
-circle.vx = (translation.x - circle.x) / 100
-circle.vy = (translation.y - circle.y) / 100
+var distX = translation.x - circle.x
+var distY = translation.y - circle.y
 
-raf = window.requestAnimationFrame(translate);
+raf = window.requestAnimationFrame(function (timestamp) {
+    starttime = timestamp || new Date().getTime()
+    translate(timestamp, circle.x, circle.y)
+});
