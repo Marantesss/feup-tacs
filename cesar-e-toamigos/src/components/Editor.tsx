384d83e12a0878610ce352677ccce8f7834f81d1
Marantesss <up201706917@edu.fe.up.pt>
2021-12-26 13:47:12 +0000

    :sparkles: add basic layout and placeholder content

--- cesar-e-toamigos/src/components/Editor.tsx
+++ cesar-e-toamigos/src/components/Editor.tsx
@@ -1,9 +1,24 @@
+import AceEditor from "react-ace";
+
 const Editor = () => {
   return (
-    <div className="h-full">
-      I am editor
-    </div>
-  )
-}
+    <>
+      <div style={{ height: "10%" }} className="flex justify-center items-center">
+        <button
+          className="p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-80 rounded-xl text-white"
+          onClick={() => console.log("clicked")}
+        >
+          Generate
+        </button>
+      </div>
+      <AceEditor
+        width="100%"
+        height="90%"
+        name="ace-code-editor"
+        editorProps={{ $blockScrolling: true }}
+      />
+    </>
+  );
+};
 
-export default Editor
+export default Editor;
--- cesar-e-toamigos/src/pages/App.tsx
+++ cesar-e-toamigos/src/pages/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import Editor from '../components/Editor';
-import Viewer from '../components/Viewer';
-import './App.css';
+import React from "react";
+import Editor from "../components/Editor";
+import GeneratedCode from "../components/GeneratedCode";
+import Viewer from "../components/Viewer";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="min-h-full w-full">
-      <div className='grid grid-cols-2 gap-2'>
+    <div className="min-h-screen w-full grid grid-cols-2 grid-rows-2 gap-2">
+      <div className="row-span-2">
         <Editor />
+      </div>
+      <div className="row-span-1">
         <Viewer />
+      </div>
+      <div className="row-span-1">
+        <GeneratedCode />
       </div>
     </div>
   );
-}
+};
 
 export default App;
