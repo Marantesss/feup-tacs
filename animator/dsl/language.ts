636af622b1aa8ade2852e73176a5394a23fa7632
Toamig <carlosmiguelvieira@hotmail.com>
2022-01-10 16:45:48 +0000

    update parser

--- animator/dsl/language.ts
+++ animator/dsl/language.ts
@@ -6,15 +6,15 @@ class Expression {
 
 class Shape extends Expression {
 
-    constructor(public id: string, public type: ShapeType, public color: Color, public size: number){
+    constructor(public id: string, public type: ShapeType, public color: Color, public position: {x: number, y: number},  public size: number, public animation: string[]){
         super();
     };
 
 };
 
-class Animation extends Expression {
+class Keyframe extends Expression {
 
-    constructor(public order: number, public target: string, public type: AnimType, public color: Color, public time: number){
+    constructor(public id: string, public type: AnimType, public color: Color, public position: {x: number, y: number}, public time: number){
         super();
     };
 
@@ -26,7 +26,7 @@ class Animation extends Expression {
 //     Color: red 
 //     Size: 25px
 
-// Animation:
+// Keyframe:
 //     Order: 1
 //     Target: 1
 //     Type: ease-in
@@ -35,10 +35,10 @@ class Animation extends Expression {
 
 
 type Grammar = {
-    expr: Shape | Animation,
+    expr: Shape | Keyframe,
 
     shapeExpr: Shape,
-    animExpr: Animation
+    keyframeExpr: Keyframe
 
     idExpr: string,
     shapeTypeExpr: string,
@@ -60,43 +60,51 @@ type Color = 'red' | 'green' | 'blue' | 'purple' | 'orange' | 'yellow' | 'pink' 
 // P.string('red'), P.string('green'), P.string('blue'), P.string('purple'), P.string('orange'), P.string('yellow'), P.string('pink'), P.string('white'), P.string('black')
 const Lang = P.createLanguage({
     expr: l => P.alt(P.seq(P.string("Shape:"), P.newline, l.shapeExpr).map(([_,__,a]) => a),
-                    P.seq(P.string("Animation:"), P.newline, l.animExpr).map(([_,__,a]) => a)).sepBy(P.newline),
-    
-    shapeExpr: l => P.alt(l.idExpr, l.shapeTypeExpr, l.colorExpr, l.sizeExpr).sepBy(P.newline).map(([a,b,c,d]) => new Shape(a,b,c,d)).skip(l.End), 
-    animExpr: l => P.alt(l.orderExpr, l.targetExpr, l.animTypeExpr ,l.colorExpr, l.timeExpr).sepBy(P.newline).map(([a,b,c,d,e]) => new Animation(a,b,c,d,e)).skip(l.End), 
+                    P.seq(P.string("Keyframe:"), P.newline, l.keyframeExpr).map(([_,__,a]) => a)).sepBy(P.newline),
 
-    idExpr: l => P.seq(P.string("id: "), l.id).map(([_,a]) => a),
-    shapeTypeExpr: l => P.seq(P.string("type: "), l.shapeType).map(([_,a]) => a),
-    animTypeExpr: l => P.seq(P.string("type: "), l.animType).map(([_,a]) => a),
-    colorExpr: l => P.seq(P.string("color: "), l.color).map(([_,a]) => a),
-    sizeExpr: l => P.seq(P.string("size: "), l.size, P.alt(P.string("px"))).map(([_,a,__]) => a),
-    orderExpr: l => P.seq(P.string("order: "), l.order).map(([_,a]) => a),
-    targetExpr: l => P.seq(P.string("target: "), l.target).map(([_,a]) => a),
-    timeExpr: l => P.seq(P.string("time: "), l.time, P.alt(P.string("s"))).map(([_,a,__]) => a),
+    shapeExpr: l => P.alt(l.subExpr).sepBy(P.newline).map(([a,b,c,d,e,f]) => new Shape(a,b,c,d,e,f)).skip(l.End), 
+    keyframeExpr: l => P.alt(l.subExpr).sepBy(P.newline).map(([a,b,c,d,e]) => new Keyframe(a,b,c,d,e)).skip(l.End), 
 
-    id: l => P.regexp(/[a-zA-Z0-9]+/),
-    shapeType: l => P.alt(P.string('square'), P.string('circle')),
-    animType: l => P.alt(P.string('linear'), P.string('ease-in'), P.string("ease-out"), P.string("ease-in-out")),
-    color: l => P.alt(P.string('red'), P.string('green'), P.string('blue'), P.string('purple'), P.string('orange'), P.string('yellow'), P.string('pink'), P.string('white'), P.string('black')),
-    size: l => P.regexp(/[0-9]+/),
-    order: l => P.regexp(/[0-9]+/), //TODO: cenas repetidas, dar cleanup depois
-    target: l=> P.regexp(/[a-zA-Z0-9]+/),
-    time: l => P.regexp(/[0-9]+/),
+
+    subExpr: l => P.seq(l.identifier.trim(P.optWhitespace).skip(P.string(":").trim(P.optWhitespace)), l.value),
+
+    identifier: l => P.alt(P.string("id"), P.string("type"), P.string("color"), P.string("position"), P.string("size"), P.string("time"), P.string("animation"), P.string("x"), P.string("y")),
+    value: l => P.alt(l.subExpr, P.regexp(/[a-zA-Z0-9]+/)), 
+
+    // idExpr: l => P.seq(P.string("id: "), l.id).map(([_,a]) => a),
+    // shapeTypeExpr: l => P.seq(P.string("type: "), l.shapeType).map(([_,a]) => a),
+    // animTypeExpr: l => P.seq(P.string("type: "), l.animType).map(([_,a]) => a),
+    // colorExpr: l => P.seq(P.string("color: "), l.color).map(([_,a]) => a),
+    // sizeExpr: l => P.seq(P.string("size: "), l.size, P.alt(P.string("px"))).map(([_,a,__]) => a),
+    // timeExpr: l => P.seq(P.string("time: "), l.time, P.alt(P.string("s"))).map(([_,a,__]) => a),
+    // positionExpr: l=> P.seq(P.string("position:"), P.newline, l.position).map(([_,__,a]) => a),
+    // animationExpr: l => P.seq(P.string("animation: "), P.alt(l.id).sepBy(P.whitespace)).map(([_,a]) => a),
+
+    // id: l => P.regexp(/[a-zA-Z0-9]+/),
+    // shapeType: l => P.alt(P.string('square'), P.string('circle')),
+    // animType: l => P.alt(P.string('linear'), P.string('ease-in'), P.string("ease-out"), P.string("ease-in-out")),
+    // color: l => P.alt(P.string('red'), P.string('green'), P.string('blue'), P.string('purple'), P.string('orange'), P.string('yellow'), P.string('pink'), P.string('white'), P.string('black')),
+    // size: l => P.digits,
+    // order: l => P.regexp(/[0-9]+/), //TODO: cenas repetidas, dar cleanup depois
+    // target: l=> P.regexp(/[a-zA-Z0-9]+/),
+    // time: l => P.regexp(/[0-9]+/),
+    position: l=> P.seq(P.string("x: "), P.digits, P.newline, P.string("y: "), P.digits).map(([_,a,__,___,b]) => [a,b]),
 
     NL: () => P.alt(P.string("\r\n"), P.oneOf("\r\n")),
 
     End: l => P.alt(l.NL, P.eof)
 });
 
-
+// Lang.positionExpr.tryParse("position:\nx: 1\ny: 2") //?
 
  //TODO: identation
-
 
 const teste = `Shape:
 id: ola
 type: circle
 color: red
+position: x: 0
+y: 0
 size: 20px
 
 Shape:
@@ -105,11 +113,13 @@ type: square
 color: purple
 size: 132px
 
-Animation:
-order: 1
-target: ola
+Keyframe:
+id: 1
 type: linear
 color: red
+position:
+x: 1
+y: 3
 time: 2s`
 
 Lang.expr.tryParse(teste); //?
