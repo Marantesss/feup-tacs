668c2b9576bd3ac497b4c355c89429e43b938e52
Marantesss <up201706917@edu.fe.up.pt>
2022-01-10 16:02:01 +0000

    :lisptick: add pretty button

--- cesar-e-toamigos/src/components/Editor.tsx
+++ cesar-e-toamigos/src/components/Editor.tsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from "react";
 import AceEditor from "react-ace";
+import cx from "classnames";
 
 const Editor = () => {
+  const [code, setCode] = useState<string>("");
+  const [isError, setIsError] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+  const compileCode = () => {
+    setIsError(false);
+    setIsSuccess(false);
+    // TODO: Change for Parsimmon compile
+    console.log(code)
+    if (code.length > 10) {
+      setIsError(true);
+    } else {
+      setIsSuccess(true);
+    }
+  };
+
+  const getButtonContent = () =>
+    (!isError && !isSuccess)
+      ? 'Compile'
+      : isError
+        ? 'Failed'
+        : 'Success'
+
+  const onEditorChange = (newValue: string) => {
+    setCode(newValue)
+    setIsError(false)
+    setIsSuccess(false)
+  }
+
   return (
     <>
       <div
@@ -8,10 +39,16 @@ const Editor = () => {
         className="flex justify-center items-center"
       >
         <button
-          className="p-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:opacity-80 rounded-xl text-white"
-          onClick={() => console.log("clicked")}
+          className={cx(
+            "p-4 w-32 bg-gradient-to-r  hover:opacity-80 rounded-xl text-white", {
+              'from-red-300 to-red-500': isError,
+              'from-green-300 to-green-500': isSuccess,
+              'from-indigo-500 via-purple-500 to-pink-500': !isError && !isSuccess,
+            }
+          )}
+          onClick={() => compileCode()}
         >
-          Generate
+          {getButtonContent()}
         </button>
       </div>
       <AceEditor
@@ -19,6 +56,8 @@ const Editor = () => {
         width="100%"
         height="90%"
         name="ace-code-editor"
+        onChange={onEditorChange}
+        value={code}
         editorProps={{ $blockScrolling: true }}
       />
     </>
